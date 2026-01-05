68ce8a5909f41572f4716cd153cb2d97a35387cb
Shark Fedora <msaed77@gmail.com>
2026-01-05 10:58:34 +0200

    add frist step in frontend

---
+++ client/app/department/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function DepartmentPage() {
+    return (
+        <div>
+            <h1>Department Page</h1>
+        </div>
+    );
+}
---
+++ client/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="p-50">
+      <h1 className="text-2xl font-bold p-30">مرحباً بكم في كورس Next.js</h1>
+      <p>هذا المشروع مرتبط بـ Node.js Backend الخاص بنا</p>
+    </main>
+
+  );
+}
